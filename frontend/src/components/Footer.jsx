3e1d31a6c723191cc5cfcae80b07a87c5f375e1a
santhoshdwarampudi <dsanthoshreddy2003@gmail.com>
2025-02-25 17:21:40 +0530

    first commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { assets } from '../assets/assets';
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+        <div>
+          <img src={assets.logo} className="mb-5 w-32" alt="Quickart Logo" />
+          <p className="w-full md:w-2/3 text-gray-600">
+            Quickart is your go-to destination for quality products at
+            unbeatable prices. Our mission is to make shopping convenient and
+            enjoyable, offering a seamless experience with fast delivery and
+            excellent customer support.
+          </p>
+        </div>
+
+        <div>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+
+        <div>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>+1-212-456-7890</li>
+            <li>support@Quickart.com</li>
+          </ul>
+        </div>
+      </div>
+
+      <div>
+        <hr />
+        <p className="py-5 text-sm text-center">
+          Copyright 2025@ Quickart.com - All Rights Reserved.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
